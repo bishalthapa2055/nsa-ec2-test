7c85bad1b950fdab758acd74df633cc48f240e63
bishalthapa2055 <bishalthapa2055@gmail.com>
2024-08-15 07:21:10 +0545

    re

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { Admin } from './model/admin';
 const start = async () => {
     console.log("Starting up........");
     const server = createServer(app);
-    const PORT = process.env.ENV_PORT || 5001;
+    const PORT = process.env.ENV_PORT || 5050;
 
     try {
         // const mongodb = config.app.mongoUrl;
